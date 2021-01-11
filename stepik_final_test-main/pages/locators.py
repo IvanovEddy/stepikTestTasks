722579293820f995831151a1608a0661933fe516
IvanovEddy <ivanov.eddy@gmail.com>
2021-01-11 21:35:07 +0300

    Добавил локаторы для страницы продукта

--- stepik_final_test-main/pages/locators.py
+++ stepik_final_test-main/pages/locators.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+
 class LoginPageLocators:
     LOGIN_ROOT = (By.CSS_SELECTOR, "#login_form")
     REGIST_ROOT = (By.CSS_SELECTOR, "#register_form")
@@ -15,3 +16,11 @@ class LoginPageLocators:
     REG_PASSWORD = (By.CSS_SELECTOR, "id_registration-password1")
     REG_PASSWORD2 = (By.CSS_SELECTOR, "id_registration-password2")
     REG_BUTTON = (By.CSS_SELECTOR, "[name='registration_submit']")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME_NOTIFICATION = (By.CSS_SELECTOR, ".alert-success:first-of-type strong")
+    BASKET_COST = (By.CSS_SELECTOR, ".alert-info strong")
+    BOOK_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    BOOK_COST = (By.CSS_SELECTOR, ".product_main .price_color")
